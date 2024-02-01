a078af23a9bcf8e77c9479043357972751682989
hoiboi <gentledemeiz@gmail.com>
2024-02-01 23:48:20 +0300

    String repeat

--- kata/src/main/java/Kata.java
+++ kata/src/main/java/Kata.java
@@ -1,21 +1,9 @@
 public class Kata {
     public static void main(String[] args) {
-        Boolean[] c = {true,  true,  true,  false,
-                true,  true,  true,  true ,
-                true,  false, true,  false,
-                true,  false, false, true ,
-                true,  true,  true,  true ,
-                false, false, true,  true};
-        System.out.println(countSheeps(c));
+        System.out.println(repeatStr(3, "lol"));
     }
 
-    public static int countSheeps(Boolean[] arrayOfSheeps) {
-        int count = 0;
-        for (Boolean arrayOfSheep : arrayOfSheeps) {
-            if (arrayOfSheep != null && arrayOfSheep) {
-                count++;
-            }
-        }
-        return count;
+    public static String repeatStr(final int repeat, final String string) {
+        return string.repeat(repeat);
     }
 }
