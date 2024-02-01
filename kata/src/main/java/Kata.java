c8fd2b5e1fd35309e2b1730010e4106ba15da025
hoiboi <gentledemeiz@gmail.com>
2024-02-02 00:18:29 +0300

    Invert values

--- kata/src/main/java/Kata.java
+++ kata/src/main/java/Kata.java
@@ -1,23 +1,19 @@
-import java.util.List;
+import java.util.Arrays;
 
 public class Kata {
     public static void main(String[] args) {
-        Object[] arr = {1, "2", 3, "4", "5"};
-        int s = sum(List.of(arr));
-        System.out.println(s);
+        int[] numbers = {1, -2, 3, -4, 5};
+        int[] additiveInverse = invert(numbers);
+        System.out.println(Arrays.toString(additiveInverse));
     }
 
-    public static int sum(List<?> mixed) {
-        int sum = 0;
+    public static int[] invert(int[] array) {
+        int[] additiveInverse = new int[array.length];
 
-        for (Object element : mixed) {
-            if (element instanceof String) {
-                sum += Integer.parseInt((String) element);
-            } else if (element instanceof Integer) {
-                sum += (int) element;
-            }
+        for (int i = 0; i < array.length; i++) {
+            additiveInverse[i] = -array[i];
         }
 
-        return sum;
+        return additiveInverse;
     }
 }
